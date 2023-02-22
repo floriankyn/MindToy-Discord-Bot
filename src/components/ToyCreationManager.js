c7b923533a8369648d9dd663290631b7f5861bd5
Florian Lepage <florian.kyn@gmail.com>
2023-02-22 03:03:20 +0100

    Update ToyCreationManager.js

--- src/components/ToyCreationManager.js
+++ src/components/ToyCreationManager.js
@@ -1,6 +1,7 @@
 //ToyCreationManager.js//
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, SelectMenuBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, ChannelType, PermissionFlagsBits, AttachmentBuilder,
-    Base
+    Base,
+    Attachment
 } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { CommandsLoader } = require("./CommandsLoader.js");
@@ -9,6 +10,10 @@ const fs = require("fs");
 const { Base64 } = require('js-base64');
 const sdk = require('api')('@scenario-api/v1.0#5lwpphle8jvsvf');
 let Initiations = new Set();
+const wait = require('util').promisify(setTimeout);
+const { createCanvas, loadImage } = require('canvas')
+const {use} = require("needle/lib/parsers");
+
 
 class ToyCreationManager{
     constructor(interaction=null, client=null, config=null) {
@@ -24,7 +29,8 @@ class ToyCreationManager{
 
     loadTables() {
         return [
-            `CREATE TABLE IF NOT EXISTS dc_creation_tokens (user_id VARCHAR(30), tokens VARCHAR(10), cooldown VARCHAR(30))`
+            `CREATE TABLE IF NOT EXISTS dc_creation_tokens (user_id VARCHAR(30), tokens VARCHAR(10), cooldown VARCHAR(30))`,
+            `CREATE TABLE IF NOT EXISTS dc_generated_toys (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, user_id VARCHAR(40), image1 TEXT, image2 TEXT, image3 TEXT, image4 TEXT, date DATETIME)`
         ];
     }
 
@@ -114,32 +120,23 @@ class ToyCreationManager{
                 )
                 .setTimestamp()
 
-        await this.interaction.followUp(
+
+
+        this.interaction.followUp(
             {
                 ephemeral: true,
+                fetchReply: true,
                 embeds: [embed]
             }
         ).then(async (interactionMessage) => {
             let imagesCreation =
                 await new Promise(async (resolve) => {
-                    let options =
-                        {
-                            parameters: {
-                                type: "txt2img",
-                                prompt: `${data.style} ${data.color1} ${data.color2} ${data.words}`
-                            },
-                            Accept: "application/json",
-                            "Content-Type": "application/json",
-                            Authorization: `Basic ${Base64.encode(`${this.config.stable_diffusion.api_key}:${this.config.stable_diffusion.api_secret}`)}`
-                        }
-
-
                     sdk.auth('Basic YXBpX0ljeWdJTGR0Unl1RlRIOVo4czFmd1E6MzAxOTQzOTQ0NmU5NTE2ODVhN2M4NzdkYjg5YWM4YTk=');
                     sdk.postModelsModelidInferences(
                         {
                             parameters: {
                                 type: 'txt2img',
-                                prompt: `${data.style} ${data.color1} ${data.color2} ${data.words}`
+                                prompt: `Minimal Abstract Art Toy, ${data.style}, ${data.color1}, ${data.color2}, ${data.words}`
                             }
                         },
                         {
@@ -150,24 +147,240 @@ class ToyCreationManager{
                     }).catch(err => console.error);
                 });
 
-            console.log(imagesCreation)
+            let imageGenerationStatus = false;
 
-            let createdImages = await new Promise(async (resolve) => {
-                setTimeout(() => {
-                    sdk.getModelsModelidInferences({modelId: 'caVLgv5XSlOF7vQTYQGwfg'})
-                        .then(({data}) => {
-                            resolve(data)
+            while (!imageGenerationStatus) {
+                await wait(3000);
+
+                let generation = await new Promise(async (resolve) => {
+                    sdk.getModelsModelidInferencesInferenceid({modelId: 'caVLgv5XSlOF7vQTYQGwfg', inferenceId: imagesCreation.inference.id})
+                        .then(({ data }) => {
+                            resolve(data);
                         })
                         .catch(err => console.error(err));
-                }, 30000)
-            });
+                });
+
+                let img = "https://miro.medium.com/max/700/1*CsJ05WEGfunYMLGfsT2sXA.gif";
+                let attachment = null;
+                let file = [];
+                let img1Buffer;
+                let img2Buffer;
+                let img3Buffer;
+                let img4Buffer;
+
+                if(generation.inference.images.length >= 1) {
+                    const canvas = createCanvas(1024, 1024);
+                    const ctx = canvas.getContext('2d')
+
+                    const img1b = createCanvas(1024, 1024)
+                    const img2b = createCanvas(1024, 1024)
+                    const img3b = createCanvas(1024, 1024)
+                    const img4b = createCanvas(1024, 1024)
+
+                    const ctxImg1b = img1b.getContext("2d")
+                    const ctxImg2b = img2b.getContext("2d")
+                    const ctxImg3b = img3b.getContext("2d")
+                    const ctxImg4b = img4b.getContext("2d")
+
+                    let generation1 = await loadImage(generation.inference.images[0].url);
+                    let generation2 = await loadImage(generation.inference.images[1].url);
+                    let generation3 = await loadImage(generation.inference.images[2].url);
+                    let generation4 = await loadImage(generation.inference.images[3].url);
+
+                    ctxImg1b.drawImage(generation1, 0, 0, 1024, 1024);
+                    ctxImg2b.drawImage(generation2, 0, 0, 1024, 1024);
+                    ctxImg3b.drawImage(generation3, 0, 0, 1024, 1024);
+                    ctxImg4b.drawImage(generation4, 0, 0, 1024, 1024);
+
+                    ctx.drawImage(generation1, 0, 512, 512, 512);
+                    ctx.drawImage(generation2, 512, 512, 512, 512);
+                    ctx.drawImage(generation3, 0, 0, 512, 512);
+                    ctx.drawImage(generation4, 512, 0, 512, 512);
+
+                    img1Buffer = img1b.toBuffer();
+                    img2Buffer = img1b.toBuffer();
+                    img3Buffer = img1b.toBuffer();
+                    img4Buffer = img1b.toBuffer();
+
+                    attachment = new AttachmentBuilder(canvas.toBuffer(), {name: "generation.png"});
+                    file.push(attachment)
+                    img = "attachment://generation.png"
+                }
 
 
-            fs.writeFileSync(`./response.json`, JSON.stringify(createdImages));
+                await this.interaction.webhook.editMessage(interactionMessage.id,
+                    {
+                        ephemeral: true,
+                        fetchReply: true,
+                        embeds: [
+                            new EmbedBuilder()
+                                .setTitle("Generation")
+                                .setColor("Blue")
+                                .setDescription(`${this.interaction.user} The images are being generated. Please wait!`)
+                                .setImage(img)
+                                .addFields(
+                                    stuff
+                                )
+                                .setFooter(
+                                    {
+                                        text: this.interaction.guild.name,
+                                        iconURL: this.interaction.guild.iconURL()
+                                    }
+                                )
+                                .setTimestamp()
+                        ],
+                        files: file
+                    }
+                ).then().catch(console.error);
 
 
+                if(generation.inference.progress === 1) {
+                    imageGenerationStatus = true;
 
+                    await this.interaction.webhook.editMessage(interactionMessage.id,
+                        {
+                            ephemeral: true,
+                            fetchReply: true,
+                            embeds: [
+                                new EmbedBuilder()
+                                    .setTitle("Generation")
+                                    .setColor("Blue")
+                                    .setDescription(`${this.interaction.user} The images are being generated. Please wait!`)
+                                    .setImage(img)
+                                    .addFields(
+                                        stuff
+                                    )
+                                    .setFooter(
+                                        {
+                                            text: this.interaction.guild.name,
+                                            iconURL: this.interaction.guild.iconURL()
+                                        }
+                                    )
+                                    .setTimestamp()
+                            ],
+                            files: file,
+                            components: [
+                                new ActionRowBuilder()
+                                    .addComponents(
+                                        new ButtonBuilder()
+                                            .setStyle("Secondary")
+                                            .setLabel("#1")
+                                            .setCustomId("image1")
+                                    )
+                                    .addComponents(
+                                        new ButtonBuilder()
+                                            .setStyle("Secondary")
+                                            .setLabel("#2")
+                                            .setCustomId("image2")
+                                    )
+                                    .addComponents(
+                                        new ButtonBuilder()
+                                            .setStyle("Secondary")
+                                            .setLabel("#3")
+                                            .setCustomId("image3")
+                                    )
+                                    .addComponents(
+                                        new ButtonBuilder()
+                                            .setStyle("Secondary")
+                                            .setLabel("#4")
+                                            .setCustomId("image4")
+                                    )
+                            ]
+                        }
+                    ).then(async (message) => {
+                        this.db.connection().getConnection(async (err, conn) => {
+                            if(err) throw err;
+
+                            let UserProfile = await this.db.query(conn, `SELECT * FROM dc_creation_tokens WHERE user_id = "${this.interaction.user.id}"`);
+
+                            let sql;
+                            if(UserProfile.length >= 1) {
+                                sql = `UPDATE dc_creation_tokens SET tokens = "${parseInt(UserProfile[0].tokens)-1}" WHERE user_id = "${this.interaction.user.id}"`;
+                            } else {
+                                sql = `INSERT INTO dc_creation_tokens (user_id, tokens) VALUES ("${this.interaction.user.id}", "4")`
+                            }
+
+                            await this.db.query(conn, sql);
+
+                            const filter = (i) => i.user.id === this.interaction.user.id;
+
+                            await this.interaction.channel.awaitMessageComponent({filter, time: 120_000})
+                                .then(
+                                    async (collected) => {
+                                        await collected.deferUpdate().then().catch(console.error);
+                                        await collected.deleteReply().then().catch(console.error);
+                                        switch (collected.customId) {
+                                            case "image1":
+                                                await this.magnifyImage(img1Buffer, conn);
+                                                break;
+                                            case "image2":
+                                                await this.magnifyImage(img2Buffer, conn);
+                                                break;
+                                            case "image3":
+                                                await this.magnifyImage(img3Buffer, conn);
+                                                break;
+                                            case "image4":
+                                                await this.magnifyImage(img4Buffer, conn);
+                                                break;
+                                        }
+                                    }
+                                )
+                                .catch(console.error);
+
+                            this.db.connection().releaseConnection(conn);
+                        })
+                    }).catch(console.error);
+                }
+            }
         }).catch(console.error);
+    }
+
+    async magnifyImage(img, conn) {
+        let attachment = new AttachmentBuilder(img, {name: "generation.png"});
+        await this.interaction.followUp(
+            {
+                ephemeral: true,
+                embeds: [
+                    new EmbedBuilder()
+                        .setImage("attachment://generation.png")
+                        .setColor("Blue")
+                        .setDescription("Wonderful! Now you can go ahead and purchase your toy and nft! Hit the payment method you'd like to use.")
+                ],
+                files: [
+                    attachment
+                ],
+                components: [
+                    new ActionRowBuilder()
+                        .addComponents(
+                            new ButtonBuilder()
+                                .setStyle("Primary")
+                                .setLabel("Paypal")
+                                .setEmoji("<:paypal:1077766385297522749>")
+                                .setCustomId("paypal")
+                        )
+                ]
+            }
+        ).then(
+            async (message) => {
+                const filter = (i) => i.user.id === this.interaction.user.id;
+
+                await this.interaction.channel.awaitMessageComponent({filter, time: 120_000})
+                    .then(
+                        async (collected) => {
+                            switch (collected.customId) {
+                                case "paypal":
+                                    await this.paypalPayment(conn);
+                                    break;
+                            }
+                        }
+                    )
+                    .catch(console.error);
+            }
+        ).catch(console.error);
+    }
+
+    async paypalPayment(conn) {
+
     }
 
     async checkCreationStatus() {
@@ -618,7 +831,17 @@ class ToyCreationManager{
         ).then().catch(console.error);
     }
 
-    async endCollector(msg) {
+    async endCollector(type) {
+        let msg;
+
+        Initiations.delete(this.interaction.user.id);
+
+        switch (type) {
+            case 1:
+                msg = `${this.interaction.user}, you took too much time to interact with the toy creation. No token have been used.`
+                break;
+        }
+
         await this.interaction.editReply(
             {
                 embeds: [
